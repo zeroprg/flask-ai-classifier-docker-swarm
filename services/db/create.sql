86fe881696310ac18b15f783e5616c5374544a3c
zeroprg <zeroprg@yahoo.com>
2020-10-24 20:46:23 +0000

    fixed compose yml for DB

--- services/db/create.sql
+++ services/db/create.sql
@@ -1,13 +1,11 @@
 CREATE DATABASE streamer;
-drop  table objects;
+--drop  table objects;
 CREATE TABLE objects(hashcode TEXT PRIMARY KEY, currentdate DATE, currentime bigint, type TEXT, frame TEXT, x_dim int, y_dim int, cam int);
-drop  table statistic;
+--drop  table statistic;
 CREATE TABLE statistic(type TEXT, currentime bigint, y int2, text TEXT, cam int2, lastime bigint, hashcodes TEXT);
 
-CREATE INDEX index_currentime ON objects(cam);
 CREATE INDEX index_currentime ON objects(currentime);
-CREATE INDEX index_currentime_stat ON statistic(cam);
 CREATE INDEX index_currentime_stat ON statistic(currentime);
-
+CREATE INDEX index_lastime_stat ON statistic(lastime);
 
 commit;
