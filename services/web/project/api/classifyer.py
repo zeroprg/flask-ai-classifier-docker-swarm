925f749f356abc983164d94cce24294ae45f9859
Vitaly Arkhipov <c-vitaly.arkhipov@charter.com>
2020-11-09 21:15:59 -0600

    fix

--- services/web/project/api/classifyer.py
+++ services/web/project/api/classifyer.py
@@ -15,7 +15,7 @@ import time
 import datetime
 import json
 from  project.api.tools.objCountByTimer import ObjCountByTimer
-from  project.config import ProductionConfig as prod 
+from  project.config import ProductionConfig as prod
 
 # initialize the list of class labels MobileNet SSD was trained to
 # detect, then generate a set of bounding box colors for each class
@@ -105,25 +105,24 @@ def classify_frame(net, frame, cam, confidence):
 
             #cv2.rectangle(frame, (startX - 25, startY - 25), (endX + 25, endY + 25), (255, 0, 0), 1)
             #cv2.putText(frame, label1, (startX - 25, startY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-            camkey = key+' '+str(cam)
             rectangle = {"startX": startX, "endX": endX, "startY": startY, "endY": endY, "text": label1}
             rectangles.append(rectangle)
-            if hashes.get(camkey, None) is None:
+            if hashes.get(key, None) is None:
                 # count objects for last sec, last 5 sec and last minute
 
-                hashes[camkey] = ImageHashCodesCountByTimer()
-                if not hashes[camkey].add(hash):
+                hashes[key] = ImageHashCodesCountByTimer()
+                if not hashes[key].add(hash):
                     continue
 
             else:
                 # if not is_hash_the_same(hash,hashes[key]): hashes[key].add(hash)
-                if not hashes[camkey].add(hash):
+                if not hashes[key].add(hash):
                     continue
                 label = ''
                 for key in hashes:
-                    if hashes[camkey].getCountedObjects() == 0:
+                    if hashes[key].getCountedObjects() == 0:
                         continue
-                    label += ' ' + key + ':' + str(hashes[camkey].getCountedObjects())
+                    label += ' ' + key + ':' + str(hashes[key].getCountedObjects())
                 topic_label = label
 
 
@@ -131,13 +130,13 @@ def classify_frame(net, frame, cam, confidence):
             if key in subject_of_interes:
                 x_dim = endX - startX
                 y_dim = endY - startY
-                #font_scale = min(y_dim, x_dim) / 300                
+                #font_scale = min(y_dim, x_dim) / 300
                 #if font_scale > 0.12:
                 #    cv2.putText(crop_img_data, str(datetime.datetime.now().strftime('%H:%M %d/%m/%y')), (1, 15),
                 #                cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 0), 1)
 
-                now = datetime.datetime.now()                
-                day = "{date:%Y-%m-%d}".format(date=now)                
+                now = datetime.datetime.now()
+                day = "{date:%Y-%m-%d}".format(date=now)
                 db.insert_frame( hash, day, int(time.time()*1000), key, crop_img_data, x_dim, y_dim, cam)
             params = do_statistic(cam, hashes)
             #db.getConn().commit()
@@ -200,7 +199,7 @@ class Trace(dict):
                           sort_keys=True, indent=4)
 
 
-def dhash(image_data, hashSize=7):
+def dhash(image_data, hashSize=8):
     image_out = np.array(image_data).astype(np.uint8)
 
     # convert the image to grayscale and compute the hash
