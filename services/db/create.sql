5d1f2ccdf973f5643fd3ab93877733ef56513fcc
zeroprg <zeroprg@yahoo.com>
2020-12-18 01:06:14 +0000

    docker files were tuned

--- services/db/create.sql
+++ services/db/create.sql
@@ -1,8 +1,25 @@
-CREATE DATABASE streamer;
-drop  table objects;
-CREATE TABLE objects(hashcode int8 PRIMARY KEY, currentdate TEXT, currentime int8, lastdate TEXT, lasttime int8, type TEXT, frame TEXT, x_dim int, y_dim int, cam int);
-drop  table statistic;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+--- drop  table  objects;
+CREATE TABLE objects(hashcode int8 PRIMARY KEY, currentdate TEXT, currentime int8, lastdate TEXT, lasttime int8, type TEXT, frame TEXT, x_dim int, y_dim int, cam int, cam_uuid uuid);
+--- drop  table  statistic;
 CREATE TABLE statistic(type TEXT, currentime int8, y int2,  cam int2, hashcodes TEXT);
+---drop  table  urls;
+CREATE TABLE urls (
+    id uuid DEFAULT uuid_generate_v4 (),
+    url VARCHAR NOT NULL,
+    cam  INT NOT NULL,
+    email VARCHAR,
+    PRIMARY KEY (id)
+);
+--- drop  table  obj_stat;
+CREATE TABLE obj_stat (
+   cam_uuid uuid NOT NULL,
+   type VARCHAR NOT NULL,
+   last_10min INT,
+   last_hour INT,
+   last_date INT
+
+);
 
 CREATE INDEX index_currentime ON objects(currentime Desc);
 CREATE INDEX index_type ON objects(type);
@@ -13,12 +30,12 @@ CREATE INDEX index_currentime_stat ON statistic(currentime Desc);
 
 
 
-CREATE OR REPLACE FUNCTION modify_last_time()
-  RETURNS TRIGGER 
-  LANGUAGE PLPGSQL
-  AS
-$$
-BEGIN
+--CREATE OR REPLACE FUNCTION modify_last_time()
+--  RETURNS TRIGGER 
+--  LANGUAGE PLPGSQL
+--  AS
+--$$
+--BEGIN
 --	IF NEW.cam = OLD.cam AND NEW.type = OLD.type AND ABS(NEW.hashcode - OLD.hashcode) < 1000 THEN
 --         IF OLD.lasttime IS NOT NULL THEN
 --    		 UPDATE objects SET currentdate = NEW.currentdate , currentime = NEW.currentime, lastdate = OLD.currentdate , lasttime = OLD.currentime WHERE cam = NEW.cam AND type = NEW.TYPE AND currentime = OLD.currentime AND ABS(NEW.hashcode - hashcode) < 30;
@@ -28,18 +45,19 @@ BEGIN
 --        RAISE EXCEPTION 'Record was updated, not inserted';
 --	END IF;
 
-	IF NEW.x_dim < 60 OR NEW.y_dim < 80  THEN
-        RAISE EXCEPTION 'Image too small';
-	END IF;
-	RETURN NEW;
-END;
-$$
+--	IF NEW.x_dim < 60 OR NEW.y_dim < 80  THEN
+---        RAISE EXCEPTION 'Image too small';
+--	END IF;
+--	RETURN NEW;
+--END;
+--$$
 
 
-CREATE TRIGGER modify_last_time
-  BEFORE INSERT
-  ON objects
-  FOR EACH ROW
-  EXECUTE PROCEDURE modify_last_time();
+--CREATE TRIGGER modify_last_time
+--  BEFORE INSERT
+--  ON objects
+--  FOR EACH ROW
+--  EXECUTE PROCEDURE modify_last_time();
 
 commit;
+
