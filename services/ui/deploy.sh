d28da51b3d967d659f698c7db2e9164594caa896
zeroprg <zeroprg@yahoo.com>
2021-01-01 20:02:07 +0000

    tune up swarm

--- services/ui/deploy.sh
+++ services/ui/deploy.sh
@@ -1,1 +1,4 @@
-sudo docker-compose build && sudo docker push zeroprg/flask-ai-classifier-docker-swarm_ui && cd ../.. &&  sudo docker stack deploy -c=docker-compose-swarm.yml flask
+git pull
+sudo docker-compose build
+sudo docker push zeroprg/flask-ai-classifier-docker-swarm_ui
+sudo docker stack deploy -c=../../docker-compose-swarm.yml flask
