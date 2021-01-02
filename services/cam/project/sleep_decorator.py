aae7bc0275a7696de775bef424b352068d07ef63
Vitaly <zeroprg@yahoo.com>
2021-01-01 21:45:29 -0700

    fix

--- services/cam/project/sleep_decorator.py
+++ services/cam/project/sleep_decorator.py
@@ -8,14 +8,14 @@ def sleep(timeout, retry=1):
     def the_real_decorator(function):
         def wrapper(*args, **kwargs):
             retries = 0
-            print(f'--------------------------------Sleeping for {timeout} seconds ----------------------------------------')
+            print('--------------------------------Sleeping for {} seconds ----------------------------------------'.format(timeout))
             while retries < retry:
                 try:
                     value = function(*args, **kwargs)
                     if value is None:
                         return
                 except Exception as e:
-                    print(f'Exception: {e}, lets sleeping for {timeout} seconds to retry')                    
+                    print('Exception: {}, lets sleeping for {} seconds to retry'.format(e,timeout))                    
                     retries += 1
 
         
