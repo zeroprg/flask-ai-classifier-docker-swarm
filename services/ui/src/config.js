1b1d69a35a2f2a878c083a09398785c6763de51e
Vitaly <zeroprg@yahoo.com>
2021-01-16 08:05:36 -0700

    fix

--- services/ui/src/config.js
+++ services/ui/src/config.js
@@ -7,5 +7,5 @@
         // rest of your translation object
     },
     // other global config variables you wish
-    API:  "//localhost:5000/" //  "//198.166.220.49:3020/",
+    API:    "//198.166.220.49:3020/", //"//localhost:5000/" 
 };
