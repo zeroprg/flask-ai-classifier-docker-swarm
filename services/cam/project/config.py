d79919c837691cf9473e8d619c494641ad0eecce
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-01-03 11:26:14 -0700

    removed sqllite

--- services/cam/project/config.py
+++ services/cam/project/config.py
@@ -31,12 +31,8 @@ class ProductionConfig:
     if DB_PORT is None or DB_PORT =='' : DB_PORT =  (  args["DB_PORT"] if ("DB_PORT" in args.keys())  else "5432")
     if DB_NAME is None or DB_NAME =='' : DB_NAME =  (  args["DB_NAME"] if ("DB_NAME" in args.keys())  else "streamer")
     if SQLALCHEMY_DATABASE_URI is None or SQLALCHEMY_DATABASE_URI=='':
-        if IP_ADDRESS is not None or args["IP_ADDRESS"] is not None:
             IP_ADDRESS =  args["IP_ADDRESS"] if ("IP_ADDRESS" in  args.keys()) else "192.168.0.167"
             SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(DB_USERNAME, DB_PASSWORD, IP_ADDRESS, DB_PORT, DB_NAME)
-        else:
-            SQLALCHEMY_DATABASE_URI = 'sqlite://frame.db'
-
     if CLASSIFIER_SERVER is None or CLASSIFIER_SERVER =='' : CLASSIFIER_SERVER =  args["CLASSIFIER_SERVER"]
     if CONFIDENCE is None or CONFIDENCE =='' : CONFIDENCE =  args["confidence"]
     if MAXIMUM_VIDEO_STREAMS is None : MAXIMUM_VIDEO_STREAMS = args["MAXIMUM_VIDEO_STREAMS"] 
