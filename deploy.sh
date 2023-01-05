0c4fe633e83dd7cd6fa5dbcbf7b110526e92b2d5
Vitaly <zeroprg@yahoo.com>
2023-01-05 14:32:04 -0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
  git pull
  cd services/cam && sh build.sh
- cd - && cd services/cam && sh build.sh
- sudo docker prune -a
+ cd - && cd services/web && sh build.sh && cd ../..
+ sudo docker image prune -a
  sudo docker service rm flask_web flask_cam
  sudo docker push zeroprg/flask-docker-swarm_cam:latest
  sudo docker push zeroprg/flask-docker-swarm_web:latest
