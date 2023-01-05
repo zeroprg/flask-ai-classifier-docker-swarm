87515e29fc5c97e49d36aa0bccba0282378abdcd
Vitaly <zeroprg@yahoo.com>
2023-01-05 14:47:15 -0700

    fixed port number in web classify url

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
  git pull
  cd services/cam && sh build.sh
  cd - && cd services/web && sh build.sh && cd ../..
- sudo docker image prune -a
+ # used to delete all images  --> sudo docker image prune -a
+ sudo docker rmi $(sudo docker images -a --filter=dangling=true -q)
  sudo docker service rm flask_web flask_cam
  sudo docker push zeroprg/flask-docker-swarm_cam:latest
  sudo docker push zeroprg/flask-docker-swarm_web:latest
