7c4b226b8ae7ce971b186902f6430f6dc92ddbc4
Vitaly <zeroprg@yahoo.com>
2023-01-09 20:21:33 -0700

    fix Docker image

--- services/ui/build.sh
+++ services/ui/build.sh
@@ -3,5 +3,5 @@ npm install
 echo $1 | sudo -S -k rm -rf build
 npm run build && 
 echo $1 | sudo -S -k npm install -g build
-
+mv build build_prepered
 sudo docker build -t zeroprg/flask-docker-swarm_ui:latest .
