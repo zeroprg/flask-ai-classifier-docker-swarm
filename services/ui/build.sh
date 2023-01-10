047ab05a457e84fdb1b2705b8cda5cc727abd84e
Vitaly <zeroprg@yahoo.com>
2023-01-09 22:26:01 -0700

    nginx

--- services/ui/build.sh
+++ services/ui/build.sh
@@ -5,3 +5,4 @@ npm run build &&
 echo $1 | sudo -S -k npm install -g build
 mv build build_prepered
 sudo docker build -t zeroprg/flask-docker-swarm_ui:latest .
+sudo docker push zeroprg/flask-docker-swarm_ui:latest
