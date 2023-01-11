26222423a4c869a3731213d46922c92895158e21
Vitaly <zeroprg@yahoo.com>
2023-01-11 14:54:15 -0700

    fixed for new network mask

--- services/ui/src/config.js
+++ services/ui/src/config.js
@@ -8,6 +8,6 @@
     },
     // other global config variables you wish
     //API:    "//198.166.220.49:3020/", //"//localhost:5000/" 
-    API:    "//192.168.1.101:3020/", //"//localhost:5000/"
+    API:    "//192.168.0.101:3020/", //"//localhost:5000/"
 };
 
