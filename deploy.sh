43f9fe846330837a4197c149b0122e1ea988bfbb
Vitaly <zeroprg@yahoo.com>
2023-01-12 15:13:14 -0700

    c

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@
  sudo docker rmi $(sudo docker images -a --filter=dangling=true -q)
  
  cd services/cam && sh build.sh
- cd - && cd services/web && sh build.sh && 
+ cd - && cd services/web && sh build.sh 
  cd - && cd services/ui  && sh build.sh && cd -
 
  sudo docker push zeroprg/flask-docker-swarm_cam:latest
