49132acb35d09bca26ba73951612d7fa008aee73
Vitaly <zeroprg@yahoo.com>
2023-01-13 16:16:43 -0700

    fix Docker file for 2 commands

--- services/cam/run.sh
+++ services/cam/run.sh
@@ -3,4 +3,5 @@
 
 # Here we will be spinning up multiple threads with multiple worker processess(-w) and perform a binding.
 
-gunicorn flaskAppServer:"create_app()" -w 4 --threads 4 -b 0.0.0.0:4000
+exec  gunicorn -w 4 -b 0.0.0.0:3020 manage:app & 
+exec  python3 videostreams.py
