73a97187176191590ff94fc8b0efc66d748bad7e
Vitaly <zeroprg@yahoo.com>
2023-01-26 00:07:21 -0700

    f

--- services/cam/run.sh
+++ services/cam/run.sh
@@ -3,5 +3,6 @@
 
 # Here we will be spinning up multiple threads with multiple worker processess(-w) and perform a binding.
 
-exec  python3 video_streams.py &
-exec  gunicorn -w 4 -b 0.0.0.0:3020 manage:app
+
+exec  gunicorn -w 4 -b 0.0.0.0:3020 manage:app &
+exec  python3 video_streams.py
