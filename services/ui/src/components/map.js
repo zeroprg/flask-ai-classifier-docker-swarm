6de9383891db990b7d625d9dbce229b11bdfbafe
Vitaly <zeroprg@yahoo.com>
2023-02-05 17:43:35 -0700

    fix map

--- services/ui/src/components/map.js
+++ services/ui/src/components/map.js
@@ -3,22 +3,29 @@ import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
 const MarkersMap = (params) => {
   const [mapCenter, setMapCenter] = useState([55.75, 37.57]);
-  //const urls = [ {lat:51.2 , lng: -112.1} , {lat:51.3  , lng: -115.2} ];
-  const [markers, setMarkers] = useState( params.markers.map(url => [url.lat, url.lng]));
+
   const [loading, setLoading] = useState(true);
-  const mapStyles = {
-    width: '100%',
-    height: '100%'
-  };
   
-  return (
+  const [error, setError] = useState(false);
+  let markers;
+  
+  if (params.markers) {
+    markers = params.markers.map(url => [url.lat, url.lng]);
+  } else {
+    setError(true);
+  }
+
+  return ( 
     <div>
     {loading && (
         <p>... Loading video stream locations on map...</p>
       )}
+    {error && (
+        <p>Error: unable to load markers data.</p>
+      )}
       
     <YMaps>
-      <Map  width="100%" height="200px" onLoad={() => {
+      <Map  width="100%" height="500px" onLoad={() => {
         setLoading(false);}}
         
         defaultState={{ center: mapCenter, zoom: 3 }}
