9416d240075a8c38f4ca3ddfd335a2605fdf2a66
Vitaly <zeroprg@yahoo.com>
2023-02-08 17:32:20 -0700

    f

--- services/ui/src/config.js
+++ services/ui/src/config.js
@@ -9,7 +9,7 @@ const config = {
         // rest of your translation object
     },
     // other global config variables you wish
-    API: "//"+process.env.STREAM_SERVER_IP +"/" //"//192.168.0.100:3020/",
+    API: "//192.168.0.100:3020/",
     //API:    "//localhost:5000/", // use it for local debuging
 };
 
