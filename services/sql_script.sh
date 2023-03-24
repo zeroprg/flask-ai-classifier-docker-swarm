dc7a8c5049e5387e61a88b84bc98a5b6a91d29ab
Vitaly <zeroprg@yahoo.com>
2023-03-24 10:48:44 -0600

    sql script to delete old records

---
+++ services/sql_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the Postgres SQL commands to be executed
+sql_commands="DELETE FROM statistic WHERE currentime < extract(epoch from now() - interval '120 hours') * 1000; DELETE FROM objects WHERE currentime < extract(epoch from now() - interval '120 hours') * 1000;"
+
+# Execute the commands using the psql command line tool
+psql -U postgres -d database_name -c "$sql_commands"
