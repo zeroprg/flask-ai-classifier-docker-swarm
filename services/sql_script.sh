884dd868216ba1d25eebc2412a3a97f202eff6ef
Vitaly <zeroprg@yahoo.com>
2023-04-15 10:29:00 -0600

    f

--- services/sql_script.sh
+++ services/sql_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Postgres SQL commands to be executed
-sql_commands="DELETE FROM statistic WHERE currentime < extract(epoch from now() - interval '120 hours') * 1000; DELETE FROM objects WHERE currentime < extract(epoch from now() - interval '120 hours') * 1000;"
+sql_commands="DELETE FROM statistic WHERE currentime < extract(epoch from now() - interval '240 hours') * 1000; DELETE FROM objects WHERE currentime < extract(epoch from now() - interval '240 hours') * 1000;"
 
 # Execute the commands using the psql command line tool
 psql -U odroid -d streamer -c "$sql_commands"
