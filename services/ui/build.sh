d73a61251138b66657126e91e98225e4486206bc
Vitaly <zeroprg@yahoo.com>
2023-04-15 11:24:27 -0600

    f

--- services/ui/build.sh
+++ services/ui/build.sh
@@ -1,4 +1,15 @@
-~/.nvm/nvm install --lts=erbium
+# Export nvm-related environment variables
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
+# Export NPM_CONFIG_PREFIX environment variable
+export NPM_CONFIG_PREFIX=~/.npm-global
+
+# Add npm global bin directory to PATH
+export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
+
 npm install
 sudo -S -k rm -rf build
 npm run build 
