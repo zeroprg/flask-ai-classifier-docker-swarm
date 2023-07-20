c61ef068bc31c4adcde1fd1406f1c6e0309ce564
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-07-20 13:44:49 -0600

    run script was fixed

--- services/web/run.sh
+++ services/web/run.sh
@@ -1,2 +1,3 @@
-sudo docker run --name web_ultralitics2  -d zeroprg/flask-docker-swarm_web:latest
+sudo docker exec -it kafka kafka-topics.sh --bootstrap_server localhost:9092 --create --topic preprocessed --partitions 1 --replication-factor 1 --config retention.ms=3600000 --config max.message.bytes=5000000 --config cleanup.policy=delete 
+sudo docker run --name web_ultralitics -e KAFKA_SERVER=loclhost:9092 -d zeroprg/flask-docker-swarm_web:latest
 #sudo docker run --name web_ultralitics2 --gpus all -d zeroprg/flask-docker-swarm_web:amd64
