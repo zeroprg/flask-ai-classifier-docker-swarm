ad0e2b91143f1bc2d9fe122a08384d736d54e9e2
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-07-23 14:35:49 -0600

    fix build files

--- services/cam/build.sh
+++ services/cam/build.sh
@@ -1,2 +1,4 @@
-sudo docker build -t zeroprg/flask-docker-swarm_cam:latest .
-sudo docker push zeroprg/flask-docker-swarm_cam:latest
+docker build --platform linux/arm64 -t zeroprg/flask-docker-swarm_cam:amd64 .
+docker build  -t zeroprg/flask-docker-swarm_cam:amd64 -f Docker.amd64 .
+docker push zeroprg/flask-docker-swarm_cam:latest
+docker push zeroprg/flask-docker-swarm_cam:amd64
--- services/web/build.sh
+++ services/web/build.sh
@@ -1,2 +1,4 @@
-sudo docker build -t zeroprg/flask-docker-swarm_web:latest .
-sudo docker push zeroprg/flask-docker-swarm_web:latest
+docker build --platform linux/arm64 -t zeroprg/flask-docker-swarm_web:amd64 .
+docker build  -t zeroprg/flask-docker-swarm_web:amd64 -f Docker.amd64 .
+docker push zeroprg/flask-docker-swarm_web:latest
+docker push zeroprg/flask-docker-swarm_web:amd64
