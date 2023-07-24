f6ee0ab8c1fcc1c5d21bd8fb121b0923b735470b
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-07-24 12:42:42 -0600

    f

--- services/web/build.sh
+++ services/web/build.sh
@@ -1,6 +1,3 @@
 docker buildx create --use
 docker buildx build  --push --platform linux/arm64 -t zeroprg/flask-docker-swarm_web:latest .
-
-docker build  -t zeroprg/flask-docker-swarm_web:amd64 -f Docker.amd64 .
-docker push zeroprg/flask-docker-swarm_web:latest
-docker push zeroprg/flask-docker-swarm_web:amd64
+docker build --push -t zeroprg/flask-docker-swarm_web:amd64 -f Docker.amd64 .
