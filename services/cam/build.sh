fd51a951c1994fd3667386990d6bbf3c7bb665da
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-07-26 16:04:34 -0600

    f

--- services/cam/build.sh
+++ services/cam/build.sh
@@ -1,2 +1,9 @@
-docker build  -t zeroprg/flask-docker-swarm_cam:latest . #--platform linux/armhf
-#docker build  --push  -t zeroprg/flask-docker-swarm_cam:amd64 -f Docker.amd64 .
+# Check the architecture and set the ARCH variable accordingly
+if [ "$(uname -m)" = "x86_64" ]; then
+  ARCH="amd64"
+  docker buildx build --platform linux/arm/v7 --push -t zeroprg/flask-docker-swarm_cam:latest .
+  docker build  --push  -t zeroprg/flask-docker-swarm_cam:amd64 -f Docker.amd64 .
+else
+  ARCH="arm"
+  docker build  -t zeroprg/flask-docker-swarm_cam:latest .
+fi
