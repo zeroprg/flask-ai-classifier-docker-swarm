f651cc691a6fdce49799751b901b55ab8f07eab2
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-07-26 14:55:42 -0600

    f

--- services/cam/build.sh
+++ services/cam/build.sh
@@ -1,4 +1,2 @@
-docker build --platform linux/arm64 -t zeroprg/flask-docker-swarm_cam:latest .
-docker build  -t zeroprg/flask-docker-swarm_cam:amd64 -f Docker.amd64 .
-docker push zeroprg/flask-docker-swarm_cam:latest
-docker push zeroprg/flask-docker-swarm_cam:amd64
+docker build --platform linux/arm32v8 --push -t zeroprg/flask-docker-swarm_cam:latest .
+docker build  --push  -t zeroprg/flask-docker-swarm_cam:amd64 -f Docker.amd64 .
