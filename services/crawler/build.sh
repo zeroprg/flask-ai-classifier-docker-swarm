8b513d71f2f3d822c87ad5377a1e90709bcf785d
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-07-28 09:20:20 -0600

    f

--- services/crawler/build.sh
+++ services/crawler/build.sh
@@ -1,8 +1,8 @@
 # Check the architecture and set the ARCH variable accordingly
 if [ "$(uname -m)" = "x86_64" ]; then
   ARCH="amd64"  
-  docker buildx build --platform linux/arm --push -t zeroprg/flask-docker-swarm_crawler:latest .
-  #docker build  --push  -t zeroprg/flask-docker-swarm_crawler:amd64 -f Docker.amd64 .
+  docker buildx build --push --platform linux/arm --push -t zeroprg/flask-docker-swarm_crawler:latest .
+  docker build  --push  -t zeroprg/flask-docker-swarm_crawler:amd64 .
 else
   ARCH="arm"
   docker build  -t zeroprg/flask-docker-swarm_crawler:latest .
