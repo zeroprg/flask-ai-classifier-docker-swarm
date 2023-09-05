1d00a1e0c85dee3b9f15c09d076aeb637707e908
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-09-04 22:32:39 -0600

    fix to armv7

--- services/web/build.sh
+++ services/web/build.sh
@@ -1,3 +1,3 @@
 docker buildx create --use
-docker buildx build  --push --platform linux/arm64 -t zeroprg/flask-docker-swarm_web:latest .
+docker buildx build  --push --platform linux/arm/v7 -t zeroprg/flask-docker-swarm_web:latest .
 docker build --push -t zeroprg/flask-docker-swarm_web:amd64 -f Docker.amd64 .
