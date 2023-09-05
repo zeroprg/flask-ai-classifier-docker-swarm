0da825fe013f24d5c25a6bca14ba57e1cc285d1c
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-09-04 23:07:16 -0600

    fix build to new version 2

--- services/ultralitics_arm32/build.sh
+++ services/ultralitics_arm32/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --output=type=image,push=true,format=docker --platform linux/arm/v7 -t zeroprg/ultralitics:arm32v7 .
+docker  build --output=type=image,push=true,format=docker --platform linux/arm/v7 -t zeroprg/ultralitics:arm32v7 .
--- services/web/build.sh
+++ services/web/build.sh
@@ -1,3 +1,3 @@
 docker buildx create --use
-docker buildx build --output=type=image,push=true,format=docker --platform linux/arm/v7 -t zeroprg/flask-docker-swarm_web:latest .
+docker build --output=type=image,push=true,format=docker --platform linux/arm/v7 -t zeroprg/flask-docker-swarm_web:latest .
 docker build --push -t zeroprg/flask-docker-swarm_web:amd64 -f Docker.amd64 .
