353bda07d4019de99d743936b3a8510d41ff2177
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-09-06 07:39:34 -0600

    multi architecrual build

--- services/ultralitics_arm32/build.sh
+++ services/ultralitics_arm32/build.sh
@@ -1,1 +1,32 @@
-docker  build --push --output=type=image,push=true,format=docker --platform linux/arm/v7 -t zeroprg/ultralitics:arm32v7 .
+#!/bin/bash
+
+# Determine the platform
+platform="$(uname -m)"
+case "$platform" in
+    x86_64)
+        docker_platform="linux/amd64"
+        tag_suffix="amd64"
+        ;;
+    armv7l)
+        docker_platform="linux/arm/v7"
+        tag_suffix="arm32v7"
+        ;;
+    *)
+        echo "Unsupported platform: $platform"
+        exit 1
+        ;;
+esac
+
+# Check if user is logged in to DockerHub
+if ! docker info | grep -q Username; then
+    echo "Please login to DockerHub first using 'docker login'"
+    exit 1
+fi
+
+# Build Docker image
+docker build --platform "${docker_platform}" -t "zeroprg/ultralitics:${tag_suffix}" .
+
+# Push the image to DockerHub
+docker push "zeroprg/ultralitics:${tag_suffix}"
+
+echo "Build and push for $platform completed!"
